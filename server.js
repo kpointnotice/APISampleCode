9412be8f0d7582b2e94e8202312fa7f16f66de1b
kpointnotice <jdw8998@daum.net>
2020-09-04 18:01:11 +0900

    WIP web.login

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const http = require('http')
+const https = require('https')
 const express = require('express')
+const fs = require('fs')
 const app = require('express')();
 const path = require('path')
 const host = '127.0.0.1';
 const port = 5000;
- 
+
 app.set('views', path.join(__dirname, '/'))
 app.set('view engine', 'html');
- 
+
 app.use(express.static('public'));
 
 //ssl 인증서 적용시
-// const options = {
-//     key  : fs.readFileSync('./ssl/knowledgetalk.key'),
-//     cert : fs.readFileSync('./ssl/knowledgetalk.pem')
-// };
- 
-http.createServer(app).listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`)
+const options = {
+    key  : fs.readFileSync('./ssl/ktgenie.key'),
+    cert : fs.readFileSync('./ssl/ktgenie_cert.pem'),
+    ca: fs.readFileSync('./ssl/RootCA.crt'),
+    passphrase: 'kpoint'
+};
+
+https.createServer(options, app).listen(port, host, () => {
+    console.log(`Server running at https://${host}:${port}/`)
 });
